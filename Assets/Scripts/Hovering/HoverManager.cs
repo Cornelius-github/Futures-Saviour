8eb9e91e8dd06e67842a25d9bd84c20cdb0179af
Connor <1022092@student.sae.edu.au>
2022-06-28 18:37:20 +1000

    Hovering Tooltips Complete
    The only change needed is to fix up the hovering tooltips so that they can't go off screen

--- Assets/Scripts/Hovering/HoverManager.cs
+++ Assets/Scripts/Hovering/HoverManager.cs
@@ -1,18 +1,51 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class HoverManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // hovertipmanager in video
+
+    public TextMeshProUGUI tipText;
+    public RectTransform tipWindow;
+
+    public static Action<string, Vector2> OnMouseHover;
+    public static Action OnMouseLoseFocus;
+
+    private void OnEnable()
     {
-        
+        OnMouseHover += ShowTip;
+        OnMouseLoseFocus += HideTip;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        OnMouseHover -= ShowTip;
+        OnMouseLoseFocus -= HideTip;
     }
+
+    void Start()
+    {
+        HideTip();
+    }
+
+    private void ShowTip(string tip, Vector2 mousePos)
+    {
+        tipText.text = tip;
+        tipWindow.sizeDelta = new Vector2(tipText.preferredWidth > 100 ? 100 : tipText.preferredWidth, tipText.preferredHeight);
+
+        tipWindow.gameObject.SetActive(true);
+        tipWindow.transform.position = new Vector2(mousePos.x + tipWindow.sizeDelta.x, mousePos.y);
+    }
+
+    private void HideTip()
+    {
+        tipText.text = default;
+        tipWindow.gameObject.SetActive(false);
+    }
+
+    //this uses actions, which are things that we can call in any part of the project
+
 }
